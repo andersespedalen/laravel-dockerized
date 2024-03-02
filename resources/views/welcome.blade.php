bcb0b686ac7cd43c79a0e16f9b19b1572e00ff1f
andersespedalen <anes@unox.no>
2024-03-02 20:56:43 +0100

    Update title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel on Google Cloud Platform</title>
+        <title>Laravel on Google Cloud Platform CI</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
